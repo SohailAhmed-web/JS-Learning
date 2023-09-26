97a3ab56ac33bece6ea98f3941b2556359e54c7a
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-09-26 23:53:20 +0500

    js 2

---
+++ Lab 05 JS/task2.js
@@ -0,0 +1,17 @@
+
+
+// Testing the parseInt function
+var int1 = parseInt("496");    
+var int2 = parseInt("53.12");  
+var int3 = parseInt("dfc374"); 
+
+console.log(int1); 
+console.log(int2); 
+console.log(int3); 
+
+// Testing the parseFloat function
+var float1 = parseFloat("90.4");    
+var float2 = parseFloat("3.14 is pi"); 
+
+console.log(float1); 
+console.log(float2); 
