bb3ba136912ae4a9eee07ad711dc2c89558be52b
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2023-09-30 22:56:18 +0500

    code for string objects

--- Lab 05 JS/task3.js
+++ Lab 05 JS/task3.js
@@ -4,3 +4,22 @@
 
 console.log(sum(4,2));
 */
+function printString(obj){
+  if(typeof obj !== 'string' || obj === null){
+    console.log("Invalid input");
+  }
+  for (const key in obj){
+    if (typeof obj[key] === 'string') {
+       console.log('${key}: ${obj[key]}');
+    }
+  }
+}
+
+const myObject = {
+  name: "Sohail",
+  age: 23,
+  address: "B3 100ft road, Sukkur",
+  email: "sohail.bscsf19@iba-suk.edu.pk"
+};
+
+printString(myObject);
